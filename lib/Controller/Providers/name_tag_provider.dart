08d2d30292b53c94c90b590521299f83d2cd1348
Manohar_nalluri <manoharnalluri47@gmail.com>
2024-01-23 11:08:23 +0530

    Changed to wizard

--- lib/Controller/Providers/name_tag_provider.dart
+++ lib/Controller/Providers/name_tag_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import '../../Services/user_preference.dart';
 
 class NameTagNotifer extends ChangeNotifier {
-  final String _name = 'Guest ';
+  final String _name = 'Wizard ';
   final String _pic = 'girl';
   String get name => UserPreference.getUserName() ?? _name;
   String get pic => UserPreference.getUserPic() ?? _pic;
