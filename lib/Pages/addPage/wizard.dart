59b6e0d4f428f1ec27c7ffcf0ae49083581d86b5
Manohar_nalluri <manoharnalluri47@gmail.com>
2024-01-23 17:03:30 +0530

    Wizard auto load bug FIX

--- lib/Pages/addPage/wizard.dart
+++ lib/Pages/addPage/wizard.dart
@@ -12,65 +12,64 @@ class WizardData extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-    return Container(
-      child: Column(children: [
-    Expanded(
-      flex: 1,
-          child: SizedBox(
-            width: width*0.9,
-            child: ListView.builder(
+    context.read<DataNotifer>().wizardEntries();
+    return Column(children: [
+      Expanded(
+        flex: 1,
+        child: SizedBox(
+          width: width * 0.9,
+          child: ListView.builder(
               reverse: true,
-                itemCount: context.watch<DataNotifer>().wizardItemsLength ,
-                itemBuilder: (context, index) {
-                  return Consumer<DataNotifer>(builder: (build, value, child) {
-                    return cardDisplay(width, value, index,true);
-                  });
-                }),
-          ),
+              itemCount: context.watch<DataNotifer>().wizardItemsLength,
+              itemBuilder: (context, index) {
+                return Consumer<DataNotifer>(builder: (build, value, child) {
+                  return cardDisplay(width, value, index, true);
+                });
+              }),
         ),
-        Container(
-            width: width * 0.9,
-            constraints: BoxConstraints(maxWidth: width * 0.9),
-            padding: const EdgeInsets.fromLTRB(
-              4,
-              6,
-              4,
-              6,
-            ),
-            decoration: BoxDecoration(
-                color: Colors.black, borderRadius: BorderRadius.circular(22)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const CircleAvatar(
-                  backgroundImage: AssetImage('assets/Logos/logo.jpg'),
-                  radius: 34,
-                ),
-                const SizedBox(
-                  width: 6,
-                ),
-                Consumer<GeminiGenNotifier>(builder: (context, value, child) {
-                  return Expanded(
-                      child: AnimatedTextKit(
-                    key: ValueKey<bool>(value.castingSpellTrue),
-                    animatedTexts: [
-                      value.castingSpellTrue
-                          ? WavyAnimatedText(value.displayWizardText,
-                              textStyle: const TextStyle(
-                                  fontSize: 16, color: Colors.white))
-                          : TyperAnimatedText(value.displayWizardText,
-                              textStyle: const TextStyle(
-                                  fontSize: 16, color: Colors.white))
-                    ],
-                    isRepeatingAnimation: false,
-                  ));
-                }),
-              ],
-            )),
-        const SizedBox(
-          height: 10,
-        )
-      ]),
-    );
+      ),
+      Container(
+          width: width * 0.9,
+          constraints: BoxConstraints(maxWidth: width * 0.9),
+          padding: const EdgeInsets.fromLTRB(
+            4,
+            6,
+            4,
+            6,
+          ),
+          decoration: BoxDecoration(
+              color: Colors.black, borderRadius: BorderRadius.circular(22)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                backgroundImage: AssetImage('assets/Logos/logo.jpg'),
+                radius: 34,
+              ),
+              const SizedBox(
+                width: 6,
+              ),
+              Consumer<GeminiGenNotifier>(builder: (context, value, child) {
+                return Expanded(
+                    child: AnimatedTextKit(
+                  key: ValueKey<bool>(value.castingSpellTrue),
+                  animatedTexts: [
+                    value.castingSpellTrue
+                        ? WavyAnimatedText(value.displayWizardText,
+                            textStyle: const TextStyle(
+                                fontSize: 16, color: Colors.white))
+                        : TyperAnimatedText(value.displayWizardText,
+                            textStyle: const TextStyle(
+                                fontSize: 16, color: Colors.white))
+                  ],
+                  isRepeatingAnimation: false,
+                ));
+              }),
+            ],
+          )),
+      const SizedBox(
+        height: 10,
+      )
+    ]);
   }
 }
